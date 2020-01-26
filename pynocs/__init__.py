b64e4769e48fc75cd2e021019dee80559fac4553
Carlo Emilio Montanari <carlidel@ymail.com>
2020-01-27 00:33:02 +0100

    improve wrapper

--- pynocs/__init__.py
+++ pynocs/__init__.py
@@ -19,6 +19,7 @@ class nocs_tools(object):
         """        
         self.engine = engine_wrapper(grid_size, random_seed)
         self.time = 0.0
+        self.lines = []
 
     def add_basic_xline(self, position):
         """Add a basic barrier to the simulation (i.e. elastic collision)
@@ -29,6 +30,7 @@ class nocs_tools(object):
             x coordinate of the barrier (must be [0, 1])
         """        
         self.engine.add_basic_xline(position)
+        self.lines.append(position)
 
     def add_fixed_xline(self, position, temperature):
         """Add a fixed barrier to the simulation (i.e. collisions reset energy to a given temperature value)
@@ -41,6 +43,7 @@ class nocs_tools(object):
             value at which energy of colliding molecules is resetted
         """        
         self.engine.add_fixed_xline(position, temperature)
+        self.lines.append(position)
 
     def add_random_xline(self, position, temperature):
         """Add a random barrier to the simulation (i.e. collisions reset energy to a random value extracted by a random exponential distribution centered at the temperature value) [TO BE CHECKED!!! ASK BEFOR USING!!!]
@@ -53,6 +56,7 @@ class nocs_tools(object):
             characteristic value of the distribution
         """
         self.engine.add_random_xline(position, temperature)
+        self.lines.append(position)
 
     def add_multiplicative_xline(self, position, elasticity):
         """Add a multiplicative barrier to the simulation (i.e. collisions multiply energy of colliding molecule with an elasticity value)
@@ -65,6 +69,7 @@ class nocs_tools(object):
             multiplicative factor for colliding molecules
         """
         self.engine.add_multiplicative_xline(position, elasticity)
+        self.lines.append(position)
 
     def add_sphere(self, x, y, radius, mass, vx, vy, tracking):
         """Add a rigid sphere to the simulation
@@ -92,10 +97,10 @@ class nocs_tools(object):
             id of the particle in the engine
         """        
         molecule_id = self.engine.add_molecule(
-            x, y,
-            np.array([0.0]), np.array([0.0]),
-            np.array([radius]), np.array([mass]),
-            vx, vy,
+            float(x), float(y),
+            [0.0], [0.0],
+            [float(radius)], [float(mass)],
+            float(vx), float(vy),
             0.0, 0.0,
             tracking
         )
@@ -133,7 +138,7 @@ class nocs_tools(object):
             dataframe with all the informations about tracked molecules
         """        
         data = self.engine.get_tracking_data()
-        df = pd.DataFrame(data, columns=['id', 'mass', 'energy', 'x', 'y', 'v_x', 'v_y'])
+        df = pd.DataFrame(data, columns=['id', 'time', 'mass', 'energy', 'x', 'y', 'v_x', 'v_y'])
         return df
 
     def load_simulation_photo(self, df, tracking_lambda=lambda x:False):
@@ -144,8 +149,18 @@ class nocs_tools(object):
         df : dataframe
             a dataframe obtained with the method get_simulation_photo()
         tracking_lambda : lambda, optional
-            since the tracking information is lost in this passage, you can rebuild it by passing a lambda function with the desired behaviour implemented, this lambda takes as argument a tuple containing the following data: tuple('mass', 'radius', 'energy', 'x', 'y', 'v_x', 'v_y'), by default lambda tuple : False
+            since the tracking information is lost in this passage, you can rebuild it by passing a lambda function with the desired behaviour implemented, this lambda takes as argument a tuple containing the following data: tuple('index', 'mass', 'radius', 'energy', 'x', 'y', 'v_x', 'v_y'), by default lambda tuple : False
         """        
         for index, row in df.iterrows():
             self.add_sphere(row['x'], row['y'], row['radius'], row['mass'], row['v_x'], row['v_y'], tracking_lambda(
-                (row['x'], row['y'], row['radius'], row['mass'], row['v_x'], row['v_y'])))
+                (index, row['x'], row['y'], row['radius'], row['mass'], row['v_x'], row['v_y'])))
+
+    def get_xline_positions(self):
+        """Return the list of positions of xlines in a list
+        
+        Returns
+        -------
+        list
+            list of x coordinates
+        """        
+        return self.lines                
